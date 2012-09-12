e58d6b54c14167c6ba0fd41223abbc14f9fad0d2
jorvis <jorvis@38e77ece-90fa-11dd-96b5-d16c606b4849>
2012-09-12 21:45:14 +0000

    now has support for user-specified ID prefixes

--- sandbox/jorvis/convert_aat_btab_to_gff3.py
+++ sandbox/jorvis/convert_aat_btab_to_gff3.py
@@ -11,6 +11,7 @@ def main():
     ## output file to be written
     parser.add_argument('-i', '--input_file', type=str, required=True, help='Path to an input file to parse' )
     parser.add_argument('-o', '--output_file', type=str, required=True, help='Path to an output file to be created' )
+    parser.add_argument('-p', '--name_prefix', type=str, required=False, help='So that resulting GFF3 files from multiple runs can be merged, you can supply a prefix for the IDs generated here' )
     args = parser.parse_args()
     algorithm = None
 
@@ -31,24 +32,28 @@ def main():
 
         if chain_num != current_chain_number:
             # this is a new chain
-            export_gene( gene_segments, ofh, current_chain_number, algorithm )
+            export_gene( gene_segments, ofh, current_chain_number, algorithm, args.name_prefix )
             current_chain_number = chain_num
             gene_segments = []
 
         gene_segments.append( segment )
     
     ## make sure to do the last one
-    export_gene( gene_segments, ofh, current_chain_number, algorithm )
+    export_gene( gene_segments, ofh, current_chain_number, algorithm, args.name_prefix )
         
 
-def get_next_id(type):
-    id = type + "{0:06d}".format( next_ids[type] )
+def get_next_id(type, prefix):
+    if prefix == None:
+        id = type + ".{0:06d}".format( next_ids[type] )
+    else:
+        id = prefix + type + ".{0:06d}".format( next_ids[type] )
+    
     next_ids[type] += 1
 
     return id
 
 
-def export_gene( segments, out, chn_num, source ):
+def export_gene( segments, out, chn_num, source, prefix ):
     contig_min = None
     contig_max = None
     contig_id  = None
@@ -73,14 +78,14 @@ def export_gene( segments, out, chn_num, source ):
     print("Exported chain: ({0}) min:({1}) max({2})".format(chn_num, contig_min, contig_max))
     
     ## write the gene feature
-    gene_id = get_next_id('gene')
+    gene_id = get_next_id('gene', prefix)
     data_column = "ID={0}".format(gene_id)
     out.write( "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format( \
             contig_id, source, 'gene', contig_min, contig_max, '.', segment_strand, '.', data_column 
             ) )
 
     ## write the mRNA
-    mRNA_id = get_next_id('mRNA')
+    mRNA_id = get_next_id('mRNA', prefix)
     data_column = "ID={0};Parent={1}".format(mRNA_id, gene_id)
     out.write( "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format( \
             contig_id, source, 'mRNA', contig_min, contig_max, '.', segment_strand, '.', data_column 
@@ -88,7 +93,7 @@ def export_gene( segments, out, chn_num, source ):
 
     ## write the exons and CDS
     for segment in segments:
-        exon_id = get_next_id('exon')
+        exon_id = get_next_id('exon', prefix)
         exon_start = min( segment['contig_start'], segment['contig_end'] )
         exon_end   = max( segment['contig_start'], segment['contig_end'] )
         data_column = "ID={0};Parent={1}".format(exon_id, mRNA_id)
@@ -97,7 +102,7 @@ def export_gene( segments, out, chn_num, source ):
                 '.', segment_strand, '.', data_column 
                 ) )
 
-        CDS_id = get_next_id('CDS')
+        CDS_id = get_next_id('CDS', prefix)
         data_column = "ID={0};Parent={1}".format(exon_id, mRNA_id)
         out.write( "{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format( \
                 contig_id, source, 'CDS', exon_start, exon_end, \
