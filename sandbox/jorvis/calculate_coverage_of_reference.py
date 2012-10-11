7bac14780f120d90785bbd72a47f75796d1c1fd7
jorvis <jorvis@38e77ece-90fa-11dd-96b5-d16c606b4849>
2012-10-11 11:27:26 +0000

    Work in progress, written initially to validate DNA-capture experiments

---
+++ sandbox/jorvis/calculate_coverage_of_reference.py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python3.2
+
+import argparse
+import math
+import os
+import re
+
+from operator import itemgetter
+
+## If a query molecule aligns to the reference at the end, this defines
+#   the cutoff for the percentage of that fragment (that isn't hanging
+#   off the end) that must align with the reference.  This should be
+#   set pretty high.
+fragment_overlap_perc_cutoff = 90
+
+def parse_annotation(gff3_file):
+    annotation = {}
+
+    for line in open(gff3_file, 'r'):
+        line.rstrip()
+        cols = line.split("\t")
+
+        if len(cols) != 9 or cols[2] != 'gene':
+            continue
+
+        asm_id = cols[0]
+
+        if asm_id not in annotation:
+            annotation[asm_id] = []
+
+        m = re.match( 'ID=(.+?)\;', cols[8] )
+        gene_id = None
+
+        if m:
+            gene_id = m.group(1)
+        else:
+            m = re.match( 'ID=(.+)', cols[8] )
+
+            if m:
+                gene_id = m.group(1)
+            else:
+                print("ERROR: failed to get a gene ID for the following line: " + line)
+
+        gene = {}
+        gene['id'] = gene_id
+        gene['fmin'] = int(cols[3]) - 1
+        gene['fmax'] = int(cols[4])
+        gene['cov'] = None
+
+        annotation[asm_id].append( gene )
+    
+    ## if debugging, report how many genes were found on each assembly
+    #for asm_id in annotation:
+        #print("DEBUG: from molecule ({0}), parsed ({1}) genes".format(asm_id, len(annotation[asm_id]) ) )
+
+    return annotation
+
+
+
+def calculate_gene_coverage( annot, frags ):
+    """
+    Iterates through the fragments aligned to a reference molecule and tracks the
+    overlaps of each fragment with the genes that are annotated on that reference
+    """
+    ## this can be reduced to a sliding analysis window if this performs unreasonably
+    for frag in frags:
+        for gene in annot:
+            #if gene['id'] == 'TP05_0045' and frag['id'] == 'ctg7180000023240':
+            #    print("DEBUG: comparing the target gene and fragment")
+            #    print("DEBUG: comparing r({0}-{1}) with q({2}-{3})".format(gene['fmin'],gene['fmax'],frag['rfmin'],frag['rfmax']) )
+
+            ## don't check this one if it's already reported as completely covered by another fragment
+            if gene['cov'] == 'complete':
+                continue
+
+            ## if the gene fmin falls within range of this fragment, it has at least partial coverage
+            if gene['fmin'] >= frag['rfmin'] and gene['fmin'] <= frag['rfmax']:
+                gene['cov'] = 'partial'
+
+                ## if the gene fmax also falls within range, the gene is fully covered
+                if gene['fmax'] <= frag['rfmax']:
+                    gene['cov'] = 'complete'
+
+            ## also check for fmax-only coverage of the gene
+            elif gene['fmax'] >= frag['rfmin'] and gene['fmax'] <= frag['rfmax']:
+                gene['cov'] = 'partial'
+
+
+def report_gene_coverage_results( annot, stats_ofh, missing_ofh ):
+    total_molecule_count = 0
+    total_gene_count = 0
+    partial_covered_gene_count = 0
+    completely_covered_gene_count = 0
+    completely_missed_gene_count = 0
+
+    for assembly_id in annot:
+        total_molecule_count += 1
+
+        for gene in annot[assembly_id]:
+            total_gene_count += 1
+
+            if gene['cov'] == 'partial':
+                partial_covered_gene_count += 1
+            elif gene['cov'] == 'complete':
+                completely_covered_gene_count += 1
+            else:
+                completely_missed_gene_count += 1
+                missing_ofh.write("assembly:{0}\t{1}\t{2}\t{3}\n".format( \
+                        assembly_id, gene['id'], gene['fmin'],gene['fmax']) )
+
+    stats_ofh.write("Total molecule count\t{0}\n".format(total_molecule_count) )
+    stats_ofh.write("Total gene count\t{0}\n".format(total_gene_count) )
+    stats_ofh.write("Genes not covered at all\t{0}\n".format(completely_missed_gene_count) )
+    stats_ofh.write("Genes covered (partially)\t{0}\n".format(partial_covered_gene_count) )
+    stats_ofh.write("Genes covered (completely)\t{0}\n".format(completely_covered_gene_count) )
+                    
+
+def calculate_fragment_coverage( ref_id, frags, ref_length, cov_stats, covinfo_ofh, ext_ofh ):
+    last_ref_coord = 0
+    last_frag_fmax = 0
+    bases_covered = 0
+    bases_uncovered = 0
+    ## these are the possible number of bases that might be extended off the end of
+    #   the reference sequence based on the fragments aligned.
+    base_extension_fmin = 0
+    base_extension_fmax = 0
+
+    ## iterate through the fragments sorted by fmin coordinate on the reference
+    for frag in sorted(frags, key=itemgetter('rfmin')):
+
+        ## does the aligned query fragment overlap the fmin of the reference?  (suggesting extension?)
+        possible_rfmin_ext = 0
+        if frag['qstrand'] == 1:
+            possible_rfmin_ext = frag['qfmin'] - frag['rfmin']
+        else:
+            possible_rfmin_ext = frag['qlen'] - frag['qfmax'] - frag['rfmin'] > 0
+
+        if possible_rfmin_ext > 0:
+            #print("DEBUG: apparent 5' overhang between {0} and {1} at frag position {2}-{3}".format(ref_id, frag['id'], frag['qfmin'], frag['qfmax']))
+            ## does it pass our alignment cutoff(s)? fragment_overlap_perc_cutoff
+            bp_aligned = frag['qfmax'] - frag['qfmin']
+            bp_unaligned_and_not_overhang = frag['qlen'] - bp_aligned - possible_rfmin_ext
+            perc_unaligned_and_not_overhang = (bp_unaligned_and_not_overhang / frag['qlen']) * 100;
+            if perc_unaligned_and_not_overhang <= (100 - fragment_overlap_perc_cutoff):
+                ext_ofh.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\n".format( \
+                        ref_id, frag['rfmin'], frag['rfmax'], 1,\
+                        frag['id'], frag['qfmin'], frag['qfmax'], frag['qstrand'], frag['qlen']))
+                #print("DEBUG: apparent 5' overhang between {0} and {1} at frag position {2}-{3}".format(ref_id, frag['id'], frag['qfmin'], frag['qfmax']))
+            
+        ## for fragments contained within a previous fragment (ex: 0-10, then 3-5)
+        if frag['rfmax'] <= last_ref_coord:
+            pass
+        ## for fragments starting after or even with the last fragment end (ex: 1-3, then 5-10 or 3-10)
+        elif frag['rfmin'] >= last_ref_coord:
+            bases_covered += (frag['rfmax'] - frag['rfmin'])
+            bases_uncovered += (frag['rfmin'] - last_ref_coord)
+            last_ref_coord = frag['rfmax']
+        ## for a new fragment overlapping the previous (ex: 1-5, then 3-8)
+        elif frag['rfmin'] < last_ref_coord:
+            bases_covered += (frag['rfmax'] - last_ref_coord)
+            last_ref_coord = frag['rfmax']
+        else:
+            raise Exception("ERROR: unhandled fragment overlap: [{0}-{1} and {2}-{3}]".format(frag['rfmin'], frag['rfmax'], frag['qfmin'], frag['qfmax']) )
+
+        last_frag_fmax = frag['rfmax']
+
+    ## then, finally, see if there's a region from the end of the last fragment to the end
+    #   of the reference
+    if last_frag_fmax < ref_length:
+        bases_uncovered += ref_length - last_frag_fmax
+
+    perc_cov = (bases_covered / ref_length) * 100
+    ## output: reference mol, ref mol size, # bases covered, percent coverage
+    covinfo_ofh.write("{0}\t{1}\t{2}\t{3}\n".format(ref_id, ref_length, bases_covered, perc_cov))
+    #print("DEBUG: {0} size: {1} has {2}bp covered, ({3:.2f}%)".format(ref_id, ref_length, bases_covered, perc_cov))
+
+    cov_stats['n_total'] += ref_length
+    cov_stats['n_cov'] += bases_covered
+    cov_stats['n_uncov'] += bases_uncovered
+    
+
+def main():
+    parser = argparse.ArgumentParser( description='Parses nucmer coords output to provide an overall coverage report')
+
+    ## coords file generated with: show-coords -l -r -T out.delta
+    parser.add_argument('-c', '--coords_file', type=str, required=True, \
+                            help='Path to a nucmer coords file with non-overlapping results (requires -l -r -T options of show-coords)' )
+    parser.add_argument('-o', '--output_prefix', type=str, required=True, help='Several output files will be created with this prefix.' )
+    parser.add_argument('-a', '--annotation_file', type=str, required=True, help='Path to a sorted GFF3 annotation file' )
+    args = parser.parse_args()
+
+    ## like: h[$assem] = [ {id=?,fmin=?,fmax=?}, ...  ]
+    annot = parse_annotation( args.annotation_file )
+
+    ## like: [ {id=?,qfmin=?,qfmax=?,rfmin=?,rfmax=?} ]
+    query_fragments = []
+
+    ## open the output files
+    genecov_stats_ofh     = open(args.output_prefix + ".stats.gene_coverage", "wt")
+    genesmissing_list_ofh = open(args.output_prefix + ".list.genes_missing", "wt")
+    refmol_stats_ofh      = open(args.output_prefix + ".stats.refmol_coverage", "wt")
+    refcov_stats_ofh      = open(args.output_prefix + ".tab.refmol_coverage", "wt")
+    refext_list_ofh        = open(args.output_prefix + ".tab.extensions", "wt")
+    refext_list_ofh.write("# {0}\n".format(args.output_prefix) )
+    refext_list_ofh.write("# reference_id\tref_fmin\tref_fmax\tref_strand\tqry_id\tqry_fmin\tqry_fmax\tqry_strand\tqry_length\n");
+    
+    ref_cov_stats = { 'n_total': 0, 'n_cov': 0, 'n_uncov': 0 }
+
+    alignment_lines_found = 0
+    current_ref_id = None
+    current_ref_length = None
+    
+    for line in open(args.coords_file, 'r'):
+        cols = line.split()
+
+        if len(cols) == 11:
+            alignment_lines_found += 1
+        else:
+            continue
+
+        cols[0] = int(cols[0])
+        cols[1] = int(cols[1])
+        cols[2] = int(cols[2])
+        cols[3] = int(cols[3])
+        
+        if cols[9] != current_ref_id:
+            if current_ref_id is not None:
+                calculate_gene_coverage( annot[current_ref_id], query_fragments )
+                calculate_fragment_coverage( current_ref_id, query_fragments, current_ref_length, ref_cov_stats, refcov_stats_ofh, refext_list_ofh )
+                
+            ## reste
+            current_ref_id = cols[9]
+            current_ref_length = int(cols[7])
+            query_fragments = []
+        
+        qstrand = 1
+
+        if cols[2] > cols[3]:
+            qstrand = -1
+
+        fragment = {}
+        fragment['id'] = cols[10]
+        fragment['qfmin']   = min(cols[2], cols[3]) - 1
+        fragment['qfmax']   = max(cols[2], cols[3])
+        fragment['qlen']    = int(cols[8])
+        fragment['qstrand'] = qstrand
+        fragment['rfmin']   = min(cols[0], cols[1]) - 1
+        fragment['rfmax']   = max(cols[0], cols[1])
+        query_fragments.append(fragment)
+
+    ## don't forget the last one
+    calculate_gene_coverage( annot[current_ref_id], query_fragments )
+    calculate_fragment_coverage( current_ref_id, query_fragments, current_ref_length, ref_cov_stats, refcov_stats_ofh, refext_list_ofh )
+    
+    if alignment_lines_found == 0:
+        raise Exception("ERROR: failed to find any 11-column alignment lines")
+    else:
+        print("INFO: {0} alignment lines found".format(alignment_lines_found) )
+
+    report_gene_coverage_results( annot, genecov_stats_ofh, genesmissing_list_ofh )
+    
+    cov_perc = (ref_cov_stats['n_cov'] / ref_cov_stats['n_total']) * 100
+    refmol_stats_ofh.write("Total bases in reference molecules\t{0}\n".format(ref_cov_stats['n_total']) )
+    refmol_stats_ofh.write("Ref bases covered by query fragments\t{0}\n".format(ref_cov_stats['n_cov']) )
+    refmol_stats_ofh.write("Ref % covered by query fragments\t{0:.2f}\n".format(cov_perc))
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
