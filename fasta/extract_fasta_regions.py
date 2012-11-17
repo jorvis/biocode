ea5012893be5fabb81ae051d13cd8eff8ae28c33
jorvis <jorvis@38e77ece-90fa-11dd-96b5-d16c606b4849>
2012-11-17 15:30:20 +0000

    First tested version of fasta/extract_fasta_regions.py

---
+++ fasta/extract_fasta_regions.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3.2
+
+'''
+Description:
+
+Accepts a tab-delimited input file that has at least molecule ID, 
+start and stop coordinates.  You must specify the column numbers
+in which each of these are found.  You can optionally pass a column
+that contains identifiers which will be used in the header of each
+sequence region extracted.
+
+Input: 
+
+- A (multi-)FASTA file.  The IDs in the header (up to the first
+whitespace) must match the identifiers in the tab file.
+- A tab file of sequence ranges to extract.  The order can be  user
+specified, but these columns must be present:
+    - molecule ID
+    - start position
+    - end position
+
+Optionally, another column can be used to provide a label for the 
+extracted range, will which appear in the FASTA header.  If not
+specified one will be fabricated from the molecule + coordinates.
+
+Column numbers provided are numbered starting at 1
+
+The coordinates in the file are assumed be of the human-readable sort
+and are internally transformed into 0-based, interbase.  A future
+feature will be to add direct support for this coordinate system.
+
+Output:
+
+Output will be multi-FASTA data printed to STDOUT or a file if you
+pass the -o option.
+
+Examples:
+
+
+'''
+
+import argparse
+import os
+import sys
+import biocodeutils
+
+
+def main():
+    parser = argparse.ArgumentParser( description='Put a description of your script here')
+
+    ## output file to be written
+    parser.add_argument('-f', '--fasta_file', type=str, required=True, help='Path to an input FASTA file' )
+    parser.add_argument('-c', '--coords_file', type=str, required=True, help='Path to a tab-delimited file with coordinates' )
+    parser.add_argument('-m', '--mol_col', type=int, required=True, help='Tabdel file column with molecule identifiers' )
+    parser.add_argument('-x', '--start_coord_col', type=int, required=True, help='Tabdel file column with coordinate start positions' )
+    parser.add_argument('-y', '--stop_coord_col', type=int, required=True, help='Tabdel file column with coordinate stop positions' )
+    parser.add_argument('-n', '--name_col', type=int, required=False, default=None, help='Optional tabdel file column with name for exported fragment' )
+    parser.add_argument('-o', '--output_file', type=str, required=False, default=None, help='Optional Path to an output file to be created' )
+    args = parser.parse_args()
+
+    ## output will either be a file or STDOUT
+    fout = sys.stdout
+    if args.output_file is not None:
+        fout = open(args.output_file, 'wt')
+    
+    seqs = biocodeutils.fasta_dict_from_file( args.fasta_file )
+
+    start_col = args.start_coord_col - 1
+    stop_col  = args.stop_coord_col - 1
+    mol_col   = args.mol_col - 1
+
+    for line in open(args.coords_file):
+        line = line.rstrip()
+        cols = line.split('\t')
+
+        if len(cols) < 3:
+            continue
+
+        (fmin, fmax, strand) = biocodeutils.humancoords_to_0interbase( int(cols[start_col]), int(cols[stop_col]) )
+        mol_id = cols[mol_col]
+
+        if mol_id not in seqs:
+            raise Exception("ERROR: molecule ID ({0}) not found in FASTA file".format(mol_id))
+
+        seq = seqs[mol_id]['s'][fmin:fmax]
+
+        seq_id = None
+        if args.name_col is None:
+            seq_id = "{0}___{1}.{2}.{3}".format( mol_id, fmin, fmax, strand  )
+        else:
+            seq_id = cols[int(args.name_col) - 1]
+
+        if strand == -1:
+            seq = biocodeutils.reverse_complement(seq)
+        
+        ## write this sequence, 60bp per line
+        fout.write(">{0}\n".format(seq_id))
+        for i in range(0, len(seq), 60):
+            fout.write(seq[i : i + 60] + "\n")
+                  
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
--- lib/biocodeutils.py
+++ lib/biocodeutils.py
@@ -1,5 +1,10 @@
 
 import os
+import re
+
+## used for nt reverse complements
+_nt_comp_table = bytes.maketrans(b'ACBDGHKMNSRUTWVYacbdghkmnsrutwvy', \
+                                 b'TGVHCDMKNSYAAWBRtgvhcdmknsyaawbr')
 
 def read_list_file( file ):
     """Parse an list file and return an array of the paths"""
@@ -17,3 +22,78 @@ def read_list_file( file ):
 
     return files
 
+
+def reverse_complement( seq ):
+    """
+    Biological reverse complementation.  Case in sequences are retained, and 
+    IUPAC codes are supported.  Code modified from:
+
+    http://shootout.alioth.debian.org/u32/program.php?test=revcomp&lang=python3&id=4
+    """
+    return seq.translate(_nt_comp_table)[::-1]
+    
+
+def humancoords_to_0interbase( start, stop ):
+    """
+    The typical human-readable coordinate system, such as found in GBK flat files,
+    has a start and stop coordinate only.  They are 1-based, on-base coordinates
+    and features on a reverse strand are indicated by having start > stop.  This
+    transforms them into the GMOD standard 0-based inter-base coordinates.
+
+    Returns a list of fmin, fmax and strand values
+    """
+    fmin = start
+    fmax = stop
+    strand = 1
+    
+    if ( stop < start ):
+        fmin = stop
+        fmax = start
+        strand = -1
+
+    fmin -= 1
+
+    return (fmin, fmax, strand)
+
+
+def fasta_dict_from_file( file ):
+    """
+    Reads a file of FASTA entries and returns a dict where each key is a sequence ID.
+    The value is another dict with two keys 'h' for header and 's' for sequence.  The
+    header is all the other text after the id in the original FASTA header.  The
+    sequence has all whitespace removed.  Obviously this should only be used on files
+    where memory to load them isn't an issue.
+    """
+    seqs = dict()
+    current_seq = ''
+    current_id = None
+    current_header = None
+    
+    for line in open(file):
+        line = line.rstrip()
+        m = re.search('>(\S+)\s*(.*)', line)
+        if m:
+            ## new residue line matched, purge the existing one, if not the first
+            if current_id is not None:
+                ## warn if it has already been found
+                if current_id in seqs:
+                    sys.stderr.write("WARN: Duplicate ID ({0}) found.  Only last one kept.".format(current_id))
+
+                ## remove all whitespace and save
+                current_seq = ''.join(current_seq.split())
+                seqs[current_id] = {'h':current_header, 's':current_seq}
+                    
+            current_seq = ''
+            current_id = m.group(1)
+            current_header = m.group(2)
+        else:
+            ## python 2.6+ makes string concatenation amortized O(n)
+            ##  http://stackoverflow.com/a/4435752/1368079
+            current_seq += str(line)
+
+    ## don't forget the last one
+    current_seq = ''.join(current_seq.split())
+    seqs[current_id] = {'h':current_header, 's':current_seq}
+
+    return seqs
+    
