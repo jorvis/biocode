ab82b6f5ef7b5a1913fe8c2175c0834e232d7fd8
jorvis <jorvis@gmail.com>
2013-12-16 01:02:20 -0600

    Added test directory and initial test - biothings range coordinate comparison

---
+++ tests/biothings_coordinate_comparisons.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+
+'''
+Loads features from a test GFF3 file and uses them to test the coordinate comparison
+functions within the biothings API.
+
+Author:  Joshua Orvis
+'''
+
+import argparse
+import os
+import biocodegff
+import biothings
+
+def main():
+    bin_dir = os.path.abspath(os.path.dirname(__file__))
+    test_gff_file = bin_dir + '/biothings_coordinate_comparisons.data'
+    
+    (assemblies, features) = biocodegff.get_gff3_features( test_gff_file )
+
+
+###########################################################################################
+
+    if features['TP03_0010'] < features['TP03_0012.t01_polypeptide']:
+        print("INFO: < positive check successful")
+    else:
+        print("ERROR: < check unsuccessful")
+
+    if features['TP03_0012'] < features['TP03_0012.t01_polypeptide']:
+        print("ERROR: < check unsuccessful")
+    else:
+        print("INFO: < negative check successful")
+
+###########################################################################################
+        
+    if features['TP03_0012'] > features['TP03_0010']:
+        print("INFO: > positive check successful")
+    else:
+        print("ERROR: > check unsuccessful")
+
+    if features['TP03_0010'] > features['TP03_0012.t01_polypeptide']:
+        print("ERROR: > check unsuccessful")
+    else:
+        print("INFO: > negative check successful")
+        
+###########################################################################################
+
+    if features['TP03_0012.t01_exon-auto15079'] <= features['TP03_0012.t01_polypeptide']:
+        print("INFO: <= positive check successful")
+    else:
+        print("ERROR: <= check unsuccessful")
+
+    if features['TP03_0010'] <= features['TP03_0012']:
+        print("ERROR: <= check unsuccessful")
+    else:
+        print("INFO: <= negative check successful")
+
+###########################################################################################
+
+    if features['TP03_0012.t01_exon-auto15085'] >= features['TP03_0012.t01_polypeptide']:
+        print("INFO: >= positive check successful")
+    else:
+        print("ERROR: >= check unsuccessful")
+
+    if features['TP03_0010'] >= features['TP03_0012']:
+        print("ERROR: >= check unsuccessful")
+    else:
+        print("INFO: >= negative check successful")
+
+###########################################################################################
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
