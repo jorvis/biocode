9f203d2d2504aa9a021cee5d05b798b4a115922f
Joshua Orvis <jorvis@gmail.com>
2014-02-05 21:19:41 -0600

    Changed script_template.py to have both -i and -o options to start (common usage)

--- script_template.py
+++ script_template.py
@@ -8,6 +8,7 @@ def main():
     parser = argparse.ArgumentParser( description='Put a description of your script here')
 
     ## output file to be written
+    parser.add_argument('-i', '--input_file', type=str, required=True, help='Path to an input file to be read' )
     parser.add_argument('-o', '--output_file', type=str, required=True, help='Path to an output file to be created' )
     args = parser.parse_args()
 
