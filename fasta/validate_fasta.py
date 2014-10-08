6a8cebd2ed87e56fd6065c3226d547e9def9db3e
Joshua Orvis <jorvis@gmail.com>
2014-10-08 14:47:12 -0500

    Added fasta/validate_fasta.py

---
+++ fasta/validate_fasta.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+'''
+Description:
+
+FASTA is a relatively easy format to get right, but sometimes accidents happen.
+This script checks the more common expectations of a FASTA file but, given that
+there's no real 'official' specification that I'm aware of, isn't perfect.
+
+Here's what it does check for:
+
+- ERROR: No > symbols embedded in sequence residues (suggests merged records)
+- ERROR: Nonzero-length identifier after > symbol until first whitespace
+- ERROR: Multiple records within an individual file with same identifier
+- ERROR: Incorrect record count (if --expected_record_count is passed)
+- ERROR: Make sure the file ends with a blank line (else commonly used tools like cat will cause errors)
+- WARNING: Residue lines > 60bp
+- WARNING: Comment lines with # (most parsers will fail to handle this)
+
+
+Input: 
+
+- One or more FASTA files passed positionally
+- A list of FASTA files passed via --input_list
+- Any combination of these
+
+
+Output:
+
+
+This will all be printed to STDOUT or a file if you pass the -o option.
+
+'''
+
+import argparse
+import os
+import re
+import sys
+import biocodeutils
+
+
+def main():
+    parser = argparse.ArgumentParser( description='Reports base/residue composition of a FASTA file')
+
+    RESIDUE_LINE_LENGTH_LIMIT = 60
+
+    ## output file to be written
+    parser.add_argument('fasta_files', metavar='N', type=str, nargs='*', help='Pass one or more FASTA files')
+    parser.add_argument('-o', '--output_file', type=str, required=False, default=None, help='Optional path to an output file to be created' )
+    parser.add_argument('-l', '--input_list', type=str, required=False, default=None, help='Optional path to a list of list of input files' )
+    parser.add_argument('-erc', '--expected_record_count', type=int, required=False, default=None, help='Option count of records expected in the input.  An exception is raised if this is not matched.' )
+    args = parser.parse_args()
+
+    ## output will either be a file or STDOUT
+    fout = sys.stdout
+    if args.output_file is not None:
+        fout = open(args.output_file, 'wt')
+
+    ## gather input
+    input_files = args.fasta_files
+
+    if args.input_list is not None:
+        input_files.extend( biocodeutils.read_list_file(args.input_list) )
+
+    if len(input_files) == 0:
+        raise Exception("ERROR: No input files defined")
+        
+    total_records = 0
+    total_residues = 0
+    error_count = 0
+    warning_count = 0
+
+    for ifile in input_files:
+        fout.write("INFO: validating file {0}\n".format(ifile))
+        line_number = 0
+        ids_found = list()
+        long_line_count = 0
+        
+        for line in open(ifile):
+            line = line.rstrip()
+            line_number += 1
+            
+            if line.startswith('>'):
+                total_records += 1
+                
+                m = re.match(">(\S+)", line)
+                if m:
+                    if m.group(1) in ids_found:
+                        fout.write("ERROR: Duplicate ID ({2}) found on line {0} of {1}\n".format(line_number, ifile, m.group(1)))
+                        error_count += 1
+                    else:
+                        ids_found.append(m.group(1))
+                else:
+                    fout.write("ERROR: Record without ID on line {0} of {1}\n".format(line_number, ifile))
+                    error_count += 1
+                
+            elif line.startswith('#'):
+                # warn about a comment line
+                fout.write("WARNING: Comment detected on line {0} of {1}\n".format(line_number, ifile))
+                warning_count += 1
+
+            else:
+                # residue line
+                total_residues += len(line)
+
+                if '>' in line:
+                    fout.write("ERROR: > character embedded in sequence residues on line {0} of {1}\n".format(line_number, ifile))
+                    error_count += 1
+
+                # not practical to print warnings for each line here, so we'll just do it once per file
+                if len(line) > RESIDUE_LINE_LENGTH_LIMIT:
+                    long_line_count += 1
+        
+        if long_line_count > 0:
+            fout.write("WARNING: {0} residue line(s) were detected longer than {1} in file {2}\n".format(long_line_count, RESIDUE_LINE_LENGTH_LIMIT, ifile))
+            warning_count += long_line_count
+
+    if args.expected_record_count is not None and args.expected_record_count != total_records:
+        fout.write("ERROR: Expected record count:{0} does not match what was found:{1}\n".format(args.expected_record_count, total_records))
+        error_count += 1
+
+    if error_count == 0:
+        if warning_count == 0:
+            fout.write("INFO: All files in input set appear to be valid\n")
+        else:
+            fout.write("INFO: All files in input set appear to be valid, but with {0} warning(s)\n".format(warning_count))
+    else:
+        fout.write("ERROR: total errors found in all files from input set: {0}, warning(s): {1}\n".format(error_count, warning_count))
+                  
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
