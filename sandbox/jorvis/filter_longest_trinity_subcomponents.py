3af1892630b3ce71137d63c43845c9da841869d4
Joshua Orvis <jorvis@gmail.com>
2014-10-30 12:18:08 -0500

    Bugfix: was writing the same ID for all output sequences!  Reported by S. Kerr

--- sandbox/jorvis/filter_longest_trinity_subcomponents.py
+++ sandbox/jorvis/filter_longest_trinity_subcomponents.py
@@ -45,7 +45,7 @@ import sys
 import biocodeutils
 
 def main():
-    parser = argparse.ArgumentParser( description='Reformats a FASTA file such that there are no more than -w characters of sequence residues per line.')
+    parser = argparse.ArgumentParser( description='Filters trinity output for longest subcomponents based on naming convention')
 
     ## output file to be written
     parser.add_argument('-i', '--input', type=str, required=True, help='Path to an input FASTA file' )
@@ -68,12 +68,13 @@ def main():
 
             if component_id not in components or len(seqs[seq_id]['s']) > len(components[component_id]['s']):
                 components[component_id] = seqs[seq_id]
+                components[component_id]['longest_id'] = seq_id
         else:
             raise Exception("ERROR: This ID wasn't in the expected format of compN_cN_seqN: {0}".format(seq_id))
 
     for c_id in components:
         seq_wrapped = biocodeutils.wrapped_fasta(components[c_id]['s'], every=60)
-        fout.write(">{0} {1}\n{2}\n".format(seq_id, components[c_id]['h'], seq_wrapped))
+        fout.write(">{0} {1}\n{2}\n".format(components[c_id]['longest_id'], components[c_id]['h'], seq_wrapped))
 
 
 if __name__ == '__main__':
