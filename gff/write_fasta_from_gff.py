c89636da904d72f1901f15c3caa38e03c4daf8e8
Joshua Orvis <jorvis@gmail.com>
2014-11-12 09:13:39 -0600

    Added --check_ends option to report non-canonical start/stops (issue #28)

--- gff/write_fasta_from_gff.py
+++ gff/write_fasta_from_gff.py
@@ -11,6 +11,17 @@ The exported IDs will be the ID attributes of the mRNA features unless these hav
 locus_tag values in the 9th column.  Also, if the polypeptides associated with each
 mRNA have product_name values, that will also be written to the FASTA header.
 
+If the --check_ends option is passed, each CDS extracted is checked for canonical start
+and stop sites.  When this happens, a warning is printed on STDERR like:
+
+   WARN: Non-canonical start codon (AAA) in mRNA G678.mRNA.9856.1
+
+Warnings are also printed if translating to protein and undefined codons are found:
+
+   WARN: Encountered unknown codon during translation: NTA
+   WARN: Non-canonical stop codon (CCA) in mRNA G678.mRNA.5532.1
+
+
 Author:  Joshua Orvis (jorvis AT gmail)
 """
 
@@ -29,6 +40,8 @@ def main():
     parser.add_argument('-o', '--output_file', type=str, required=False, help='Path to an output FASTA file to be created' )
     parser.add_argument('-t', '--type', type=str, required=False, default='protein', choices=['protein', 'cds'], help='Type of features to export')
     parser.add_argument('-f', '--fasta', type=str, required=False, help='If the FASTA entries for the underlying assemblies is absent from the GFF3 document passed, you will need to specify this option' )
+    parser.add_argument('--check_ends', dest='check_ends', action='store_true')
+    parser.set_defaults(check_ends=False)
     args = parser.parse_args()
 
     ## output will either be a file or STDOUT
@@ -37,6 +50,10 @@ def main():
         fout = open(args.output_file, 'wt')
 
     (assemblies, features) = biocodegff.get_gff3_features(args.input_file)
+
+    # only doing the standard codon table for now
+    start_codons = ['ATG', 'GTG', 'TTG']
+    stop_codons  = ['TAG', 'TAA', 'TGA']
 
     ## add sequence residues from external FASTA file if the user passed one
     if args.fasta is not None:
@@ -67,6 +84,16 @@ def main():
                     fout.write("\n")
                 
                 coding_seq = mRNA.get_CDS_residues()
+
+                if args.check_ends == True:
+                    # check the starting codon
+                    start_codon = coding_seq[0:3].upper()
+                    if start_codon not in start_codons:
+                        sys.stderr.write("WARN: Non-canonical start codon ({0}) in mRNA {1}\n".format(start_codon, mRNA.id))
+
+                    stop_codon = coding_seq[-3:].upper()
+                    if stop_codon not in stop_codons:
+                        sys.stderr.write("WARN: Non-canonical stop codon ({0}) in mRNA {1}\n".format(stop_codon, mRNA.id))                        
 
                 if args.type == 'cds':
                     fout.write("{0}\n".format(biocodeutils.wrapped_fasta(coding_seq)))
