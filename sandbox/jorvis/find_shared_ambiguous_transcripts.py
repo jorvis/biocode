7e9530ac40b940f18ac19692970572bee12ae43a
Joshua Orvis <jorvis@gmail.com>
2015-02-10 10:45:53 -0600

    Progress commit

--- sandbox/jorvis/find_shared_ambiguous_transcripts.py
+++ sandbox/jorvis/find_shared_ambiguous_transcripts.py
@@ -44,6 +44,26 @@ PROCESS:
 
 counts[cluster_id][$range_label] = N
 
+NOTES:
+Strand-specific read library alignment info:
+A10_IL100044639 against IL100038440 transcripts
+A1_IL100044635 against IL100038432_IL100038433 transcripts
+A3_IL100044636 against IL100038435 transcripts
+A7_IL100044637 against IL100038438 transcripts
+A8_IL100044638 against IL100038439 transcripts
+
+Test cluster:
+192796  C: 1    A: 1    D: 1    B: 1    N/A: 4
+S	192796	617	*	*	*	*	*	IL100038709_IL100038708_comp126419_c12_seq3 len=617 path=[166718850:0-306 166547606:307-337 166548086:338-616]	*
+H	192796	343	100.0	+	0	0	263I343M11I	IL100038432_comp86601_c2_seq1 len=343 path=[69567882:0-190 69568797:191-342]	IL100038709_IL100038708_comp126419_c12_seq3 len=617 path=[166718850:0-306 166547606:307-337 166548086:338-616]
+H	192796	319	100.0	+	0	0	263I319M35I	IL100038441_comp4006_c0_seq1 len=319 path=[2:0-74 627:75-318]	IL100038709_IL100038708_comp126419_c12_seq3 len=617 path=[166718850:0-306 166547606:307-337 166548086:338-616]
+C	192796	3	100.0	*	*	*	*	IL100038709_IL100038708_comp126419_c12_seq3 len=617 path=[166718850:0-306 166547606:307-337 166548086:338-616]	*
+
+IL100038709_IL100038708_comp126419_c12_seq3
+IL100038432_comp86601_c2_seq1
+IL100038441_comp4006_c0_seq1
+IL100038709_IL100038708_comp126419_c12
+
 """
 
 import argparse
@@ -70,17 +90,17 @@ def main():
     ## mapping file: A3_reorientation.midrange.0.05-0.95.id.list : comp1893_c0_seq1
     sample_labels = {
         'IL100038432': 'A1',
-        'IL100038432_IL100038433': 'A1',
-        'IL100038434': 'A2',
+        #'IL100038432_IL100038433': 'A1',
+        #'IL100038434': 'A2',
         'IL100038435': 'A3',
-        'IL100038436': 'A5',
+        #'IL100038436': 'A5',
         'IL100038438': 'A7',
         'IL100038439': 'A8',
-        'IL100038440': 'A10',
-        'IL100038441': 'A10',
-        'IL100038709': 'A1',
-        'IL100038709_IL100038708': 'A1',
-        'IL100038721': 'B6'
+        'IL100038440': 'A10'
+        #'IL100038441': 'A10',
+        #'IL100038709': 'A1',
+        #'IL100038709_IL100038708': 'A1',
+        #'IL100038721': 'B6'
     }
 
     # structure like
@@ -123,8 +143,10 @@ def main():
     # Now check the membership of each cluster, then print the counts
     for cluster_id in cluster_members:
         ofh.write("{0}\t".format(cluster_id))
+        # to track those cluster members which weren't part of the read alignment experiment
+        na_count = 0
         
-        for range_label in ranges:
+        for range_label in sorted(ranges):
             transcript_count = 0
 
             for transcript_id in cluster_members[cluster_id]:
@@ -135,16 +157,24 @@ def main():
                 else:
                     raise Exception("ERROR: transcript ID didn't follow expected convention: {0}".format(transcript_id))
 
-                sample_name = sample_labels[sample_id]
+                if sample_id in sample_labels:
+                    sample_name = sample_labels[sample_id]
+                else:
+                    na_count += 1
+                    continue
 
                 if sample_name not in targets:
                     continue
+
+                # to make things match up, the sample_id needs to be prefixed onto the transcript_id
+                transcript_id = "{0}_{1}".format(sample_id, transcript_id)
 
                 if transcript_id not in midrange_members[sample_name][range_label]:
                     transcript_count += 1
 
             ofh.write("{0}: {1}\t".format(range_label, transcript_count))
 
+        ofh.write("N/A: {0}".format(na_count))
         ofh.write("\n")
 
                     
