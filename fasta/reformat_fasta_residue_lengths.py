38e1c72cde326ef97b025c249a6bc2cd743f7af8
Joshua Orvis <jorvis@gmail.com>
2015-10-13 09:09:09 -0500

    Added --upper_case option

--- fasta/reformat_fasta_residue_lengths.py
+++ fasta/reformat_fasta_residue_lengths.py
@@ -25,6 +25,7 @@ def main():
     parser.add_argument('-i', '--input', type=str, required=True, help='Path to an input FASTA file' )
     parser.add_argument('-w', '--width', type=int, required=False, default=60, help='Width - number of residues per line' )
     parser.add_argument('-o', '--output', type=str, required=False, help='Output file to be created.  Default = STDOUT' )
+    parser.add_argument('-uc', '--upper_case', action='store_true', required=False, help='Forces all bases to be upper-case' )
     args = parser.parse_args()
 
     ## output will either be a file or STDOUT
@@ -35,6 +36,9 @@ def main():
     seqs = biocodeutils.fasta_dict_from_file( args.input )
 
     for seq_id in seqs:
+        if args.upper_case == True:
+            seqs[seq_id]['s'] = seqs[seq_id]['s'].upper()
+            
         seq_wrapped = biocodeutils.wrapped_fasta(seqs[seq_id]['s'], every=args.width)
         fout.write(">{0} {1}\n{2}\n".format(seq_id, seqs[seq_id]['h'], seq_wrapped))
 
