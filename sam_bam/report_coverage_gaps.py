497cf02669e85bbbe0bd2bf63fefb72770eb4211
jorvis <jorvis@gmail.com>
2017-01-03 10:57:36 -0600

    Added support for ignoring gaps near the end of molecules

--- sam_bam/report_coverage_gaps.py
+++ sam_bam/report_coverage_gaps.py
@@ -38,11 +38,12 @@ def main():
     parser = argparse.ArgumentParser( description='Report coverage gaps/dips from a samtools mpileup file')
 
     ## output file to be written
-    parser.add_argument('-i', '--input_file', type=str, required=True, help='Path to an input file to be read' )
+    parser.add_argument('-i', '--input_file', type=str, required=True, help='Path to an input mpileup file to be read' )
     parser.add_argument('-o', '--output_file', type=str, required=False, help='Path to an output file to be created' )
     parser.add_argument('-f', '--fasta_file', type=str, required=True, help='Reference fasta file, against which reads were aligned.  Needed for low 3-prime end coverage' )
     parser.add_argument('-mcd', '--min_coverage_depth', type=int, required=True, help='Min coverage depth, below which is reported' )
     parser.add_argument('-mcs', '--min_coverage_span', type=int, required=False, help='Coverage window size, the avg of which is calculated for depth cutoff' )
+    parser.add_argument('-eb', '--end_buffer', type=int, required=False, default=0, help='If specified, gaps this far from either end of the molecule will not be reported.' )
     args = parser.parse_args()
 
     # Check, this isn't ready yet:
@@ -72,20 +73,21 @@ def main():
             
             # purge the last one
             if current_seq_id != None:
-                print_spans(current_seq_id, depths, args.min_coverage_depth, out_fh, stats)
+                print_spans(current_seq_id, depths, args.min_coverage_depth, out_fh, stats, lengths, args.end_buffer)
 
             depths = [0] * lengths[contig]
             current_seq_id = contig
 
         depths[this_coord - 1] = depth
 
-    print_spans(current_seq_id, depths, args.min_coverage_depth, out_fh, stats)
+    print_spans(current_seq_id, depths, args.min_coverage_depth, out_fh, stats, lengths, args.end_buffer)
 
     print("INFO: Total molecules: {0}".format(stats['total_molecules']), file=sys.stderr)
     print("INFO: Total bases    : {0}".format(stats['total_bases']), file=sys.stderr)
     print("INFO: Avg cov depth  : {0}x".format(int(stats['depth_sum'] / stats['total_bases'])), file=sys.stderr)
      
-def print_spans(id, depths, cutoff, fh, stats):
+def print_spans(id, depths, cutoff, fh, stats, lengths, skip_end_dist):
+    print("Skip end dist is: {0}".format(skip_end_dist))
     i = 0
     span_start = None
     in_span = False
@@ -100,7 +102,16 @@ def print_spans(id, depths, cutoff, fh, stats):
                 span_start = i
         else:
             if in_span == True:
-                fh.write("{0}\t{1}\t{2}\t{3}\n".format(id, span_start + 1, i + 1, i - span_start))
+                if skip_end_dist is not None and skip_end_dist > 0:
+                    print("DEBUG: ended first span within {0}, skipped end needs evaluating".format(id))
+                    if i > skip_end_dist and i < (lengths[id] - skip_end_dist):
+                        print("\tDEBUG: span with i:{0} falls within range to include".format(id))
+                        fh.write("{0}\t{1}\t{2}\t{3}\n".format(id, span_start + 1, i + 1, i - span_start))
+                    else:
+                        print("\tDEBUG: span with i:{0} falls within range to exclude".format(id))
+                else:
+                    print("DEBUG: ended first span within {0}, skipped end doesn't need evaluating".format(id))
+                    fh.write("{0}\t{1}\t{2}\t{3}\n".format(id, span_start + 1, i + 1, i - span_start))
 
             in_span = False
             span_start = None
