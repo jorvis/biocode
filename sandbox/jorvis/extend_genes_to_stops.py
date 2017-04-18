c6ce139d27f33a5c7e619f915772dbdecdc337ab
jorvis <jorvis@gmail.com>
2017-04-18 09:49:06 -0500

    Now corrects for phase issues when doing extension

--- sandbox/jorvis/extend_genes_to_stops.py
+++ sandbox/jorvis/extend_genes_to_stops.py
@@ -56,13 +56,16 @@ def main():
                     mRNA_loc = mRNA.location_on(assemblies[assembly_id])
                     
                     CDSs = sorted(mRNA.CDSs())
+                    CDS_frame_overhang = len(coding_seq) % 3
                     codon_step_size = 3
 
                     if mRNA_loc.strand == 1:
-                        CDS_pos = CDSs[-1].location_on(assemblies[assembly_id]).fmax
+                        # get the in-frame end coordinate of the last CDS position
+                        CDS_pos = CDSs[-1].location_on(assemblies[assembly_id]).fmax - CDS_frame_overhang
                         mRNA_limit = mRNA_loc.fmax + mRNA_extension_limit
                     else:
-                        CDS_pos = CDSs[0].location_on(assemblies[assembly_id]).fmin
+                        # get the in-frame end coordinate of the last CDS position
+                        CDS_pos = CDSs[0].location_on(assemblies[assembly_id]).fmin + CDS_frame_overhang
                         mRNA_limit = mRNA_loc.fmin - mRNA_extension_limit
                         codon_step_size = -3
 
@@ -80,9 +83,15 @@ def main():
                             break
                         else:
                             next_codon = assemblies[assembly_id].residues[CDS_pos:CDS_pos + 3]
+                            
+                            if mRNA_loc.strand == -1:
+                                next_codon = utils.reverse_complement(next_codon)
+
                             print(".{0}({1})".format(next_codon, CDS_pos), end='')
                         
                             if next_codon in stop_codons:
+                                mRNA.extend_stop(on=assemblies[assembly_id], to=CDS_pos)
+
                                 new_stop_found = True
                                 print(" Found a stop")
                                 break
@@ -99,6 +108,9 @@ def main():
     print("\nTotal mRNAs found:{0}".format(total_mRNAs))
     print("mRNAs initially with terminal stops: {0}".format(mRNAs_with_terminal_stops))
     print("mRNAs which can be corrected: {0}".format(mRNAs_corrected))
+
+    ofh = open(args.output_gff, 'wt')
+    gff.print_gff3_from_assemblies(assemblies=assemblies, ofh=ofh)
 
 
 if __name__ == '__main__':
