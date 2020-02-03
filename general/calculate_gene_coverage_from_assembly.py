a773dfad072f189b68617468134c75bd7346aba7
Joshua Orvis <jorvis@gmail.com>
2020-02-03 13:52:06 -0600

    Added script to calculate gene coverage when one assembly is mapped to another

--- general/calculate_gene_coverage_from_assembly.py
+++ general/calculate_gene_coverage_from_assembly.py
@@ -2,171 +2,170 @@
 
 """
 
-Original use case description:
+The biocode script calculate_coverage_of_reference.py uses a base-level approach at
+reporting on the coverage of genes in a reference.  This doesn't work for some
+use cases, as reported by J. Da Silva:
 
 Specifically, if a gene contains a highly variable region, this will cause the alignment 
 to break. So, even though the gene is there completely, because there is no alignment 
-containing a fraction of the gene, that fraction is assumed to be missing in the new assembly.
+containing a fraction of the gene, that fraction is assumed to be missing in the new 
+assembly.
 
 Here is the fix I propose. If the matches to the start and end of a gene in the reference 
-genome are both in a single contig in the new assembly, that gene should be considered to 
-be present in its entirety, even if some parts are not in an alignment.
+genome are both in a single contig in the new assembly, that gene should be considered 
+to be present in its entirety, even if some parts are not in an alignment.
 
-This is a demo to test this.
+---------------------------------------------
 
-Input nucmer coords format:
-[S1] start of the alignment region in the reference sequence 
-[E1] end of the alignment region in the reference sequence 
-[S2] start of the alignment region in the query sequence 
-[E2] end of the alignment region in the query sequence 
-[LEN 1] length of the alignment region in the reference sequence 
-[LEN 2] length of the alignment region in the query sequence 
-[% IDY] percent identity of the alignment 
-[LEN R] length of the reference sequence 
-[LEN Q] length of the query sequence 
+This script uses this simpler approach.  Ouput classes for each gene:
 
-Output:
+unmapped - Neither end of the gene was found within any of the newly-mapped contigs.
+split_mapping - Both ends were mapped but on separate contigs in the new assembly
+fully_mapped - Both ends were mapped on the same contig in the new assembly
+3_prime_mapped_only - You've got this.
+5_prime_mapped_only - This one too.
 
-The output is a 2-column tab-delimited file where the first column is the gene ID
-and the second is it's coverage class.  Here are the possible values for those:
+Example input:
 
-single_contig_full_coverage - Both sides of the gene are covered by the same 
-aligned contig.
+[S1]    [E1]    [S2]    [E2]    [LEN 1] [LEN 2] [% IDY] [LEN R] [LEN Q] [TAGS]
+1       255     75      328     255     254     92.40   2540030 11810   tp.assembly.567468735.1 NODE_38_length_11810_cov_73.2673
+3       481     1583618 1583156 479     463     91.74   2540030 1583618 tp.assembly.567468735.1 NODE_1_length_1583618_cov_74.0429
+1732    2389    1759    2413    658     655     90.90   2540030 3677    tp.assembly.567468735.1 NODE_60_length_3677_cov_38.595
+2210    9290    1253    8326    7081    7074    94.18   2540030 11810   tp.assembly.567468735.1 NODE_38_length_11810_cov_73.2673
+10092   10694   8552    9154    603     603     92.74   2540030 11810   tp.assembly.567468735.1 NODE_38_length_11810_cov_73.2673
 
-multi_contig_full_coverage - Both sides of the are covered, but by different
-aligned contigs.
+Validation:
 
-fmax_coverage - Only the right-most side of the gene (unstranded) has coverage.
+Gene:FFF274896AFEDB774BE80BF4026117B0 on tp.assembly.567497685.1 at 548145-549821
+	matching NODE_12_length_224693_cov_77.5294 at 547008-548379
+	matching NODE_2_length_910331_cov_77.0225 at 548104-548472
 
-fmin_coverage - Only the left-most side of the gene (unstranded) has coverage.
-
-no_coverage - Neither side of this gene overlaps an aligned contig.
-
+tp.assembly.567497685.1	.	gene	548146	549821	.	+	.	ID=FFF274896AFEDB774BE80BF4026117B0
+547008	548379	22890	21508	1372	1383	90.46	570487	224693	tp.assembly.567497685.1	NODE_12_length_224693_cov_77.5294
+548104	548472	15028	14645	369	384	82.73	570487	910331	tp.assembly.567497685.1	NODE_2_length_910331_cov_77.0225
 """
 
 import argparse
 import os
-
-from biocode import gff, things
-
-DEBUG_COORDINATES = False
+from biocode import utils, gff, things
+import sys
 
 def main():
-    parser = argparse.ArgumentParser( description='Put a description of your script here')
-    parser.add_argument('-c', '--coords_file', type=str, required=True, \
-                            help='Path to a nucmer coords file with non-overlapping results (requires -l -r -T options of show-coords)' )
-    parser.add_argument('-a', '--annotation_file', type=str, required=True, help='Path to a sorted GFF3 annotation file' )
-    parser.add_argument('-r', '--reference_fasta', type=str, required=True, help='Path to the reference file used with nucmer' )
- 
+    parser = argparse.ArgumentParser( description='Gene coverage calculator')
+
+    parser.add_argument('-c', '--coords', type=str, required=True, help='Coords file, probably from a nucmer alignment' )
+    parser.add_argument('-a', '--annotation_file', type=str, required=True, help='GFF3 annotation of input' )
+    parser.add_argument('-r', '--reference_fasta', type=str, required=True, help='Reference FASTA from GFF3 coordinates' )
+    parser.add_argument('-p', '--protein_encoding_only', action='store_true', help='Pass if you only want protein-coding genes included' )
+    parser.add_argument('-d', '--debugging', action='store_true', help='If passed, prints debugging output on STDERR' )
     args = parser.parse_args()
 
     (assemblies, features) = gff.get_gff3_features(args.annotation_file)
 
-    # matches[ref_id][qry_id]
-    matches = parse_nucmer_matches(assemblies, args.coords_file)
+    matches = load_matches(args.coords, assemblies)
 
     for assembly_id in assemblies:
         assembly = assemblies[assembly_id]
 
-        for gene in assembly.genes():
-            start_covered = False
-            end_covered = False
-            full_single_contig_coverage = False
-
-            if DEBUG_COORDINATES:
-                print("gene:{0}\tat coordinates {1}-{2}".format(gene.id, gene.location().fmin, gene.location().fmax))
-            else:
-                print(gene.id, end='\t')
-
-            # check that the assembly has any matches at all
-            if assembly_id in matches:
-                for contig_id in matches[assembly_id]:
-                    if DEBUG_COORDINATES: print("\tchecking matches from contig: {0}".format(contig_id))
-                    start_covered_by_this_contig = False
-                    end_covered_by_this_contig = False
-
-                    for match in matches[assembly_id][contig_id].parts:
-                        if DEBUG_COORDINATES: print("\t\t{0}-{1}".format(match.location().fmin, match.location().fmax))
-                        if gene.overlaps_with(match):
-                            if DEBUG_COORDINATES: print("\t\t\toverlaps")
-                            # is it completely contained
-                            if gene.contained_within(match):
-                                if DEBUG_COORDINATES: print("\t\t\tcontained_within")
-                                start_covered_by_this_contig = True
-                                end_covered_by_this_contig = True
-
-                            # ok, what about the start side?
-                            elif match.overlaps_min_side_of(gene):
-                                start_covered = True
-                                start_covered_by_this_contig = True
-
-                            elif match.overlaps_max_side_of(gene):
-                                end_covered = True
-                                end_covered_by_this_contig = True
-
-                    # no more comparisons need to happen. report the type of this one
-                    if start_covered_by_this_contig and end_covered_by_this_contig:
-                        full_single_contig_coverage = True
-                        break
-
-            if full_single_contig_coverage:
-                print("single_contig_full_coverage")
-            elif start_covered and end_covered:
-                print("multi_contig_full_coverage")
-            elif start_covered:
-                print("fmin_coverage")
-            elif end_covered:
-                print("fmax_coverage")
-            else:
-                print("no_coverage")
-    
-def parse_nucmer_matches(assemblies, match_file):
-    # [ref_id][qry_id] = Match
-    matches = dict()
-    
-    with open(match_file) as fh:
-        in_alignment_region = False
-        next_match_num = 1
+        # some contigs have no mappings
+        if assembly_id not in matches: continue
+        match = matches[assembly_id]
         
-        for line in fh:
-            if line.startswith('[S1]'):
-                in_alignment_region = True
-                continue
+        for gene in assembly.genes():
+            if args.protein_encoding_only:
+                if len(gene.mRNAs()) == 0:
+                    if args.debugging:
+                        print("DEBUG: Skipping non-encoding gene {0}".format(gene.id), file=sys.stderr)
+                    continue
+            
+            fmin_matches = list()
+            fmax_matches = list()
+            gene_loc = gene.location_on(assembly)
 
-            if in_alignment_region:
-                line = line.rstrip()
-                cols = line.split()
-                ref_id = cols[9]
-                qry_id = cols[10]
+            if args.debugging:
+                print("DEBUG: Gene:{0} on {3} at {1}-{2}".format(gene.id, gene_loc.fmin, gene_loc.fmax, gene_loc.on.id), file=sys.stderr)
 
-                if int(cols[0]) < int(cols[1]):
-                    ref_start = int(cols[0]) - 1
-                    ref_end = int(cols[1])
-                    ref_strand = 1
-                else:
-                    ref_start = int(cols[1]) - 1
-                    ref_end = int(cols[0])
-                    ref_strand = -1
+            for mp in match.parts:
+                match_loc = mp.location_on(assembly)
+                
+                if mp.overlaps_min_side_of(thing=gene):
+                    if args.debugging:
+                        print("\tmatching {0} at {1}-{2}".format(mp.id, match_loc.fmin, match_loc.fmax), file=sys.stderr)
+                        
+                    fmin_matches.append(mp.id)
 
-                if ref_id not in matches:
-                    matches[ref_id] = dict()
+                if mp.overlaps_max_side_of(thing=gene):
+                    if args.debugging:
+                        print("\tmatching {0} at {1}-{2}".format(mp.id, match_loc.fmin, match_loc.fmax), file=sys.stderr)
+                        
+                    fmax_matches.append(mp.id)
 
-                if qry_id not in matches[ref_id]:
-                    match = things.Match(id="match_{0}".format(next_match_num), subclass='match')
-                    next_match_num += 1
-                    matches[ref_id][qry_id] = match
+            # everything starts as unmapped
+            match_class = 'unmapped'
 
-                # check assumptions
-                if ref_start > ref_end:
-                    raise Exception("Error, unexpected ref_start ({0}) > ref_end ({1})".format(ref_start, ref_end))
+            # if there were matches to both ends the class gets upgraded to at least this
+            if len(fmin_matches) and len(fmax_matches):
+                match_class = 'split_mapping'
 
-                # each line is a MatchPart
-                match_part = things.MatchPart(parent=matches[ref_id][qry_id], length=cols[8])
-                match_part.locate_on(target=assemblies[ref_id], fmin=ref_start, fmax=ref_end, strand=ref_strand)
-                matches[ref_id][qry_id].add_part(match_part)
+                # see if there was a shared match between the two ends
+                if shared_match(fmin_matches, fmax_matches):
+                    match_class = 'fully_mapped'
+
+            if match_class == 'unmapped':
+                # let's see if at least one of the ends is mapped
+                if len(fmin_matches):
+                    if gene_loc.strand == 1:
+                        match_class = '5_prime_mapped_only'
+                    else:
+                        match_class = '3_prime_mapped_only'
+
+                if len(fmax_matches):
+                    if gene_loc.strand == 1:
+                        match_class = '3_prime_mapped_only'
+                    else:
+                        match_class = '5_prime_mapped_only'
+
+            print("{0}\t{1}\t{2}".format(assembly.id, gene.locus_tag, match_class))
+                        
+            
+def shared_match(a, b): 
+    a_set = set(a) 
+    b_set = set(b) 
+    if len(a_set.intersection(b_set)) > 0: 
+        return(True)  
+    return(False)                    
+            
+                
+def load_matches(infile, assemblies):
+    matches = dict()
+
+    for line in open(infile):
+        line = line.rstrip()
+        cols = line.split("\t")
+
+        if len(cols) < 9: continue
+        # this matches header lines
+        if line[0] == '[': continue
+
+        ref_asmbl_id = cols[9]
+        new_asmbl_id = cols[10]
+        ref_start = int(cols[0])
+        ref_end   = int(cols[1])
+        
+        if ref_start > ref_end:
+            raise Exception("Unexpected start {0} > {1} end".format(ref_start, ref_end))
+
+        if ref_asmbl_id not in matches:
+            #matches[ref_asmbl_id] = list()
+            matches[ref_asmbl_id] = things.Match()
+
+        #matches[ref_asmbl_id].append({'start': ref_start, 'end': ref_end, 'mol_id': new_asmbl_id})
+        mp = things.MatchPart(id=new_asmbl_id, parent=matches[ref_asmbl_id])
+        mp.locate_on(target=assemblies[ref_asmbl_id], fmin=ref_start, fmax=ref_end)
+        matches[ref_asmbl_id].add_part(mp)
 
     return matches
-
+        
 if __name__ == '__main__':
     main()
 
