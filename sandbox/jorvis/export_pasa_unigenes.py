85226d9a6000afed2881bb98944596a118eaad5e
Joshua Orvis <jorvis@gmail.com>
2020-09-03 22:56:29 -0500

    Execution is much more modular now

--- sandbox/jorvis/export_pasa_unigenes.py
+++ sandbox/jorvis/export_pasa_unigenes.py
@@ -28,6 +28,10 @@ Assumes transdecoder output looks like this ('longest_orfs.cds'):
    >asmbl_6.p1 type:5prime_partial len:126 asmbl_6:2055-1678(-)
    >asmbl_6.p2 type:complete len:104 asmbl_6:374-685(+)
 
+Example command:
+
+./export_pasa_unigenes.py -if /usr/local/projects/aplysia/PASA/20200511_polca_triway/sample_mydb_pasa.sqlite.assemblies.fasta -ig /usr/local/projects/aplysia/PASA/20200511_polca_triway/sample_mydb_pasa.sqlite.pasa_assemblies.gtf -it /usr/local/projects/aplysia/transdecoder/longest_orfs.cds -is /usr/local/projects/aplysia/salmon/salmon_out/quant.sf
+
 """
 
 import argparse
@@ -41,12 +45,10 @@ def main():
     parser = argparse.ArgumentParser( description='Export the transcript from each PASA cluster with the longest ORF')
     parser.add_argument('-if', '--input_pasa_fasta', type=str, required=True, help='Path to PASAs predicted FASTA file' )
     parser.add_argument('-itsc', '--input_pasa_transcript_size_cutoff', type=str, required=False, default=300, help='Nucleotide size cutoff of input transcripts' )
-    parser.add_argument('-itc', '--input_pasa_tpm_cutoff', type=str, required=False, default=0.1, help='TPM cutoff of input transcripts' )
+    parser.add_argument('-itc', '--input_pasa_tpm_cutoff', type=float, required=False, default=0.1, help='TPM cutoff of input transcripts' )
     parser.add_argument('-ig', '--input_pasa_gtf', type=str, required=True, help='Path to PASAs predicted GFT file' )
     parser.add_argument('-it', '--input_transdecoder', type=str, required=True, help='Path to the longest_orfs.cds from transdecoder' )
     parser.add_argument('-is', '--input_salmon', type=str, required=True, help='Path to quant.sf file from Salmon' )
-    parser.add_argument('-ol', '--output_file_longest_orf', type=str, required=True, help='Path to an output file to be created' )
-    parser.add_argument('-oa', '--output_file_most_abundant', type=str, required=True, help='Path to an output file to be created' )
     args = parser.parse_args()
 
     seqs = utils.fasta_dict_from_file(args.input_pasa_fasta)
@@ -66,16 +68,55 @@ def main():
     print("Initial PASA cluster count: {0}".format(pasa_cluster_count))
     print("Initial PASA transcript count: {0}".format(pasa_transcript_count))
     
-    seqs, pasa_clusters = apply_transcript_size_cutoff(seqs, pasa_clusters, args.input_pasa_transcript_size_cutoff)
-    seqs, pasa_clusters = apply_abundance_filter(seqs, pasa_clusters, args.input_pasa_tpm_cutoff)
+    #seqs, pasa_clusters = apply_transcript_size_cutoff(seqs, pasa_clusters, args.input_pasa_transcript_size_cutoff)
+    
     seqs, pasa_clusters = apply_orf_filter(seqs, pasa_clusters, orf_lengths, 100)
+    write_unigenes(seqs, pasa_clusters, 'pasa.orf_filtered', orf_lengths)
+    
+    seqs, pasa_clusters = apply_abundance_filter(seqs, pasa_clusters, args.input_pasa_tpm_cutoff)
+    write_unigenes(seqs, pasa_clusters, 'pasa.orf_and_tpm_0.03_filtered', orf_lengths)
 
+def write_unigenes(seqs, clusters, prefix, orf_lengths):
+    orf_unigenes_fh = open("{0}.orf_unigenes.fasta".format(prefix), 'wt')
+
+    for cluster_id in clusters:
+        max_orf_length = 0
+        max_orf_id = None
+
+        for asmbl_id in clusters[cluster_id]:
+            if orf_lengths[asmbl_id] > max_orf_length:
+                max_orf_length = orf_lengths[asmbl_id]
+                max_orf_id = asmbl_id
+
+        orf_unigenes_fh.write(">{0} {1}\n{2}\n".format(max_orf_id, seqs[max_orf_id]['h'], 
+                                              utils.wrapped_fasta(''.join(seqs[max_orf_id]['s']))))
+    
+    orf_unigenes_fh.close()
+
+    tpm_unigenes_fh = open("{0}.tpm_unigenes.fasta".format(prefix), 'wt')
+
+    for cluster_id in clusters:
+        max_tpm = 0
+        max_orf_id = None
+
+        for asmbl_id in clusters[cluster_id]:
+            if 'tpm' in seqs[asmbl_id]:
+                if seqs[asmbl_id]['tpm'] > max_tpm:
+                    max_tpm = seqs[asmbl_id]['tpm']
+                    max_orf_id = asmbl_id
+
+        if max_orf_id:
+            tpm_unigenes_fh.write(">{0} {1}\n{2}\n".format(max_orf_id, seqs[max_orf_id]['h'], 
+                                              utils.wrapped_fasta(''.join(seqs[max_orf_id]['s']))))
+    
+    tpm_unigenes_fh.close()
+    
 def write_transcripts(seqs, filename):
     with open(filename, 'wt') as fh:
         for asmbl_id in seqs:
             fh.write(">{0} {1}\n{2}\n".format(asmbl_id, seqs[asmbl_id]['h'], 
                                               utils.wrapped_fasta(''.join(seqs[asmbl_id]['s']))))
-            
+
 def apply_orf_filter(seqs, clusters, orf_lengths, orf_length_cutoff):
     clusters_to_delete = list()
     
@@ -100,8 +141,8 @@ def apply_orf_filter(seqs, clusters, orf_lengths, orf_length_cutoff):
     print("\tTranscripts: {0}".format(len(seqs)))
     print("\tPASA clusters: {0}".format(len(clusters)))
 
-    write_transcripts(seqs, 'pasa.orf_filtered.fasta')
-    
+    write_transcripts(seqs, 'pasa.orf_filtered.all_cluster_members.fasta')
+
     return seqs, clusters
     
 def apply_abundance_filter(seqs, clusters, tpm_cutoff):
@@ -129,8 +170,8 @@ def apply_abundance_filter(seqs, clusters, tpm_cutoff):
     print("\tTranscripts: {0}".format(len(seqs)))
     print("\tPASA clusters: {0}".format(len(clusters)))
 
-    write_transcripts(seqs, 'pasa.tpm_filtered.fasta')
-                    
+    write_transcripts(seqs, 'pasa.tpm_filtered.all_cluster_members.fasta')
+
     return seqs, clusters
 
 def apply_transcript_size_cutoff(seqs, clusters, size_cutoff):
@@ -157,7 +198,7 @@ def apply_transcript_size_cutoff(seqs, clusters, size_cutoff):
     print("\tTranscripts: {0}".format(len(seqs)))
     print("\tPASA clusters: {0}".format(len(clusters)))
 
-    write_transcripts(seqs, 'pasa.size_filtered.fasta')
+    write_transcripts(seqs, 'pasa.size_filtered.all_cluster_members.fasta')
                     
     return seqs, clusters
     
