bb0a42a0955df2a380daae0fe4ef0fd2504a0765
Joshua Orvis <jorvis@gmail.com>
2021-12-07 09:31:27 -0600

    Corrected missing module scope within gff3.print_gff3_from_assemblies() call

--- lib/biocode/gff.py
+++ lib/biocode/gff.py
@@ -579,7 +579,7 @@ def print_gff3_from_assemblies(assemblies=None, ofh=None, source=None):
                     gene.remove_mRNA(mRNA)
                     
                     print("INFO: splitting mRNA off gene {0}".format(gene.id))
-                    new_gene = things.Gene(id="{0}_{1}".format(gene.id, rnas_found))
+                    new_gene = biocode.things.Gene(id="{0}_{1}".format(gene.id, rnas_found))
                     new_gene.locate_on(target=current_assembly, fmin=mRNA_loc.fmin, fmax=mRNA_loc.fmax, strand=mRNA_loc.strand)
                     new_gene.add_RNA(mRNA)
                     new_gene.print_as(fh=ofh, format='gff3', source=source)
