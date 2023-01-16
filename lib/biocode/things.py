b2020b07b68cf5b11debb67fb5f80bf7fb7eeef1
Christopher Holt <chrisholt97@gmail.com>
2023-01-16 13:18:52 -0500

    Changed function to keep introns that annotated in augutus.hints.gff3 file, instead of prediciting introns

--- lib/biocode/things.py
+++ lib/biocode/things.py
@@ -1213,35 +1213,35 @@ class RNA( LocatableThing ):
             return False
 
     def introns(self, on=None):
-        
-         return self.children["intron"] ## This ports port all the introns straight from Augustus
 
-        # '''
-        # Dynamically generates Intron objects in order for the current RNA.  The coordinates of the
-        # generated introns depend on the object passed via the 'on' argument
-        # '''
-        # if on is None:
-        #     raise Exception("ERROR: the introns() method requires a passed molecule using the 'on' argument")
+        return self.children["intron"] ## This ports port all the introns straight from Augustus
 
-        # mol_on = on
+            # '''
+            # Dynamically generates Intron objects in order for the current RNA.  The coordinates of the
+            # generated introns depend on the object passed via the 'on' argument
+            # '''
+            # if on is None:
+            #     raise Exception("ERROR: the introns() method requires a passed molecule using the 'on' argument")
 
-        # intron_objs = list()
-        # last_exon = None
-        # last_exon_loc = None
+            # mol_on = on
 
-        # for exon in sorted(self.exons()):
-        #     exon_loc = exon.location_on( mol_on )
+            # intron_objs = list()
+            # last_exon = None
+            # last_exon_loc = None
 
-        #     if last_exon is not None:
-        #         intron_id = uuid.uuid4() ## ## This is generating random id, need format such as "g1.t1.intron1"
-        #         intron = Intron( id=intron_id )
-        #         intron.locate_on( target=mol_on, fmin=last_exon_loc.fmax, fmax=exon_loc.fmin, strand=exon_loc.strand )
-        #         intron_objs.append( intron )
+            # for exon in sorted(self.exons()):
+            #     exon_loc = exon.location_on( mol_on )
 
-        #     last_exon = exon
-        #     last_exon_loc = exon_loc
+            #     if last_exon is not None:
+            #         intron_id = uuid.uuid4() ## ## This is generating random id, need format such as "g1.t1.intron1"
+            #         intron = Intron( id=intron_id )
+            #         intron.locate_on( target=mol_on, fmin=last_exon_loc.fmax, fmax=exon_loc.fmin, strand=exon_loc.strand )
+            #         intron_objs.append( intron )
 
-        # return intron_objs
+            #     last_exon = exon
+            #     last_exon_loc = exon_loc
+
+            # return intron_objs
 
     def polypeptides(self):
         return self.children['polypeptide']
