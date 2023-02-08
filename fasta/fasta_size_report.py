c32da3ec01c3af36b6844a0e0579e4dbcdb06d6f
RPINerd <rpinerd@gmail.com>
2023-02-08 18:39:59 -0500

    Set up the framework and bulk of the translation

--- fasta/fasta_size_report.py
+++ fasta/fasta_size_report.py
@@ -52,29 +52,78 @@ import argparse
 import os
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Prints a synopsis file containing the ID, the size of the sequence, the original defline, and the originating file')
+def main(args):
 
-    # Input file options
-    parser.add_mutually_exclusive_group()
-    parser.add_argument('-f', '--file', type=str, required=True, help='Path to an input file to be read' )
-    parser.add_argument('-o', '--output_file', type=str, required=True, help='Path to an output file to be created' )
-    parser.add_argument('-o', '--output_file', type=str, required=True, help='Path to an output file to be created' )
+    # Enable logging if option set
+    # TODO if log, redirect print to file
+    if args.log:
+        log_file = open(args.log, "w")
 
-    # Optional flags
-    parser.add_argument('-l', '--log', required=False, help='Generate a log file if set', action="store_true")
-    parser.add_argument('-s', '--summary', required=False, help='Generate summary lines in report', action="store_true")
-    args = parser.parse_args()
+    # Convert file argument into parsable list
+    file_list = handle_file_input(args)
 
-    # Your code goes here
+    # Process input fasta file(s)
+    records = []
+    for fa in file_list:
+
+        # Check for valid file, but skip gracefully if missing
+        if not os.path.exists(fa):
+            print(f"The fasta file {fa} was not found! Skipping..")
+            continue
+
+        records.append(parse_file(fa))
+
+    # Print metrics
+    for record in records:
+        print("\t".join(record))
+
+
+def parse_file(fasta):
+
+    record = []
+
+    return record
+
+
+def handle_file_input(args):
+
+    file_list = []
+
+    if args.dir:
+
+        assert os.path.exists(args.dir), f"Directory not found at location {args.dir}"
+
+        for file in os.listdir(args.dir):
+            if file.endswith(".fa", ".fasta"):
+                file_list.append(file)
+
+    elif args.list:
+
+        assert os.path.exists(args.list), f"List file {args.list} was not found!"
+
+        with open(args.list, "r") as fa_list:
+            for line in fa_list:
+                file_list.append(line.strip())
+
+    else:
+        file_list.append(args.file.split(" "))
+
+    return file_list
 
 
 if __name__ == '__main__':
-    main()
 
+    parser = argparse.ArgumentParser(description='Prints a synopsis file containing the ID, the size of the sequence, the original defline, and the originating file')
 
+    # Input file options
+    input_files = parser.add_mutually_exclusive_group()
+    input_files.add_argument('-f', '--file', type=str, required=True, help='Path to an input file to be read' )
+    input_files.add_argument('-l', '--list', type=str, required=True, help='Path to a file containing a list of fastas' )
+    input_files.add_argument('-d', '--dir', type=str, required=True, help='Path to a directory containing fasta files' )
 
+    # Optional flags
+    parser.add_argument('-l', '--log', required=False, help='Generate a log file if set')
+    parser.add_argument('-s', '--summary', required=False, help='Generate summary lines in report', action="store_true")
+    args = parser.parse_args()
 
-
-
-
+    main(args)
