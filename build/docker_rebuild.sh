9686bb1bac95ede7eea2a84d194b538a8dd6279c
Joshua Orvis <jorvis@gmail.com>
2023-11-17 00:02:45 -0600

    Updated docker image for 0.11.0

--- build/docker_rebuild.sh
+++ build/docker_rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This should match the PyPi version, which needs to be available first.
-IMAGE_VERSION='0.10.0'
+IMAGE_VERSION='0.11.0'
 
 docker rmi -f jorvis/biocode
 docker build --no-cache -t jorvis/biocode:latest -t jorvis/biocode:${IMAGE_VERSION} .
